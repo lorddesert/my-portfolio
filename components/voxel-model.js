769f5fc3db4b8ea3a6925bbc6d81420a3db7c35d
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-05 14:42:12 -0300

    add 3d voxel model

---
+++ components/voxel-model.js
@@ -0,0 +1,22 @@
+import { useState, useEffect, useRef, useCallback } from 'react'
+import { Box, Spinner} from '@chakra-ui/react'
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { loadGLTFModel } from '../libs/model'
+
+function easeOutCirc(x) {
+    return Math.sqrt(1 - Math.pow(x - 1, 4))
+}
+
+const VoxelModel = () => {
+    const refContainer = useRef()
+
+    return (
+        <Box ref={refContainer}>
+            Voxeeeeeeeeel!
+        </Box>
+    )
+}
+
+export default VoxelModel
