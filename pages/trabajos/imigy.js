44cbc504732eb0e697125a767860506080b2ca89
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-05 18:17:39 -0300

    imigy page added

---
+++ pages/trabajos/imigy.js
@@ -0,0 +1,41 @@
+import { Container, Badge, Link, List, ListItem, AlertTitle } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+
+import P from '../../components/paragraph'
+
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+    return (
+        <Layout title="Search'n Rock">
+            <Container>
+                <Title>
+                    Imigy <Badge>2021 - Presente</Badge>
+
+                </Title>
+                <P>Aplicacion web minimalista para compartir fotos y screenshots con nuestros amigos hecho con Svelte.</P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://imigy.vercel.app">
+                        https://imigy.vercel.app
+                        <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <Meta>Plataforma</Meta>
+                    <span>Web</span>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>Svelte, Bulma CSS, Firebase, Figma</span>
+                    </ListItem>
+                </List>
+                <WorkImage src="/images/imigy.png" />
+                <WorkImage src="/images/imigy-2.png" />
+                <WorkImage src="/images/imigy-3.png" />
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
