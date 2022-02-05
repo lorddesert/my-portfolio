4c1eeb1de8e5a179513cb353adf7dce30d7c35f0
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-05 14:43:19 -0300

    add trabajos page

---
+++ pages/trabajos.js
@@ -0,0 +1,37 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
+
+import thumbImigy from '../public/images/imigy.png'
+import thumbSearchNRock from '../public/images/search-n-rock.png'
+
+const Works = () => {
+ return (
+  <Layout>
+   <Container>
+    <Heading as="h3" fontSize={20} mb={4}>
+     Trabajos
+    </Heading>
+    <SimpleGrid columns={[1, 1, 2]} gap={6}>
+     <Section>
+      <WorkGridItem id="imigy" title="Imigy" thumbnail={thumbImigy}>
+       Aplicación para postear y compartir imagenes con tus amigos.
+      </WorkGridItem>
+     </Section>
+     <Section>
+      <WorkGridItem
+       id="search-rock"
+       title="Search n' rock"
+       thumbnail={thumbSearchNRock}
+      >
+       Aplicación para buscar información sobre tus bandas de rock favoritas.
+      </WorkGridItem>
+     </Section>
+    </SimpleGrid>
+   </Container>
+  </Layout>
+ )
+}
+
+export default Works
