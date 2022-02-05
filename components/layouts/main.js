a7f9705dcd1f8af9c23f7e9ce2b7461dac5a14a4
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-04 21:24:45 -0300

    change SEO

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,17 +1,22 @@
 import Head from 'next/head'
+import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
-const Main = ({ children }) => {
+const Main = ({ children}) => {
+    const router = useRouter()
  return (
   <Box as="main" pb={8}>
    <Head>
     <meta charset="UTF-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Music === Code." />
+    <meta name="description" content="Front end DEV, UI Designer" />
     <link rel="icon" type="image/x-icon" href="/images/logo.png" />
-    <title>Lorddesert&apos;s house.</title>
+    <title>Sacha Nocetto - Home</title>
    </Head>
+
+   <Navbar path={router.asPath} />
 
    <Container maxW="container.md" pt={14}>
        {children}
