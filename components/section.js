23ae87c5ff88f923488e701a69899be08df99bc7
Sacha Nocetto <sacha.nocetto@hotmail.com>
2022-02-06 21:01:43 -0300

    add id prop

--- components/section.js
+++ components/section.js
@@ -7,13 +7,14 @@ const StyledDiv = chakra(motion.div, {
  }
 })
 
-const Section = ({ children, delay = 0 }) => {
+const Section = ({ children, delay = 0, id }) => {
  return (
   <StyledDiv
    initial={{ y: 10, opacity: 0 }}
    animate={{ y: 0, opacity: 1 }}
    transition={{ duration: 0.8, delay }}
    mb={6}
+   id={id}
   >
    {children}
   </StyledDiv>
