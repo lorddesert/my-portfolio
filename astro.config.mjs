e8e951de99a93a045013ad37283924e4950539b5
lorddesert <sacha.nocetto@hotmail.com>
2024-05-12 20:26:03 -0300

    add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        '/posts': '/blog'
-      }
+  redirects: {
+    '/posts': '/blog'
+  },
+  output: 'server',
+  adapter: vercel(),
 });
